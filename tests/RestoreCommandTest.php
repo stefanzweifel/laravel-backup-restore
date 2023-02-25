637f1718afbced8ec64bcf68643f1c6ff62dcc8c
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-02-25 14:37:46 +0100

    Add Tests

---
+++ tests/RestoreCommandTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Wnx\LaravelBackupRestore\Commands\RestoreCommand;
+
+it('downloads remote backup, imports a single mysql dump into mysql database', function () {
+    $this->artisan(RestoreCommand::class, [
+        '--disk' => 'remote',
+        '--backup' => 'Laravel/2023-01-28-mysql-no-compression-no-encryption.zip',
+        '--database' => 'mysql',
+    ])->assertSuccessful();
+
+    $result = DB::connection('mysql')->table('users')->count();
+
+    expect($result)->toBe(10);
+
+});
+
+it('downloads remote backup, decrypts and imports a single mysql dump into mysql database', function () {
+    $this->artisan(RestoreCommand::class, [
+        '--disk' => 'remote',
+        '--backup' => 'Laravel/2023-01-28-mysql-no-compression-encrypted.zip',
+        '--database' => 'mysql',
+        '--password' => 'password',
+    ])->assertSuccessful();
+
+    $result = DB::connection('mysql')->table('users')->count();
+
+    expect($result)->toBe(10);
+});
+
+it('downloads and decompresses remote backup, imports a single mysql dump into mysql database', function () {
+    $this->artisan(RestoreCommand::class, [
+        '--disk' => 'remote',
+        '--backup' => 'Laravel/2023-01-28-mysql-compression-no-encryption.zip',
+        '--database' => 'mysql',
+    ])->assertSuccessful();
+
+    $result = DB::connection('mysql')->table('users')->count();
+
+    expect($result)->toBe(10);
+})->skip('TODO: Implement decompression');
+
+it('downloads, decompresses remote backup, decrypts and imports a single mysql dump into mysql database', function () {
+    $this->artisan(RestoreCommand::class, [
+        '--disk' => 'remote',
+        '--backup' => 'Laravel/2023-01-28-mysql-compression-encrypted.zip',
+        '--database' => 'mysql',
+        '--password' => 'password',
+    ])->assertSuccessful();
+
+    $result = DB::connection('mysql')->table('users')->count();
+
+    expect($result)->toBe(10);
+})->skip('TODO: Implement decompression');
