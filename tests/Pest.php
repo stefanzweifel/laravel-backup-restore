1b657b4cbb469d5cc3d6ab2491ca6486752b8d4a
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-02-25 14:59:04 +0100

    Clear temp directory

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,11 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
 use Wnx\LaravelBackupRestore\Tests\TestCase;
 use function Pest\Laravel\artisan;
 
 
 uses(TestCase::class)
     ->beforeEach(function () {
+        // Delete all files in the temp directory
+        Storage::disk('local')->deleteDirectory('backup-restore-temp');
+
+
         // Wipe all databases before each test
         artisan('db:wipe', [
             '--database' => 'mysql',
@@ -17,6 +22,9 @@ uses(TestCase::class)
         artisan('db:wipe', [
             '--database' => 'mysql',
         ]);
+
+         // Delete all files in the temp directory
+         Storage::disk('local')->deleteDirectory('backup-restore-temp');
     })
     ->in(__DIR__);
 
