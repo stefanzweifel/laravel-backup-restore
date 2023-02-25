6b1176239d088df87f5422cab779e909b12f06a3
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-02-25 14:37:39 +0100

    Wipe Databases before and after each test

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,24 @@
 <?php
 
 use Wnx\LaravelBackupRestore\Tests\TestCase;
+use function Pest\Laravel\artisan;
 
-uses(TestCase::class)->in(__DIR__);
+
+uses(TestCase::class)
+    ->beforeEach(function () {
+        // Wipe all databases before each test
+        artisan('db:wipe', [
+            '--database' => 'mysql',
+        ]);
+
+    })
+    ->afterEach(function () {
+        // Wipe all databases after each test
+        artisan('db:wipe', [
+            '--database' => 'mysql',
+        ]);
+    })
+    ->in(__DIR__);
+
+
+
