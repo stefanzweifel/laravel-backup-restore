baf2fb10ac9d3fc79e51e18b7c21992f8681add9
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-04 16:12:14 +0100

    Use Dataset in RestoreCommandTest

--- tests/RestoreCommandTest.php
+++ tests/RestoreCommandTest.php
@@ -5,58 +5,37 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\DB;
 use Wnx\LaravelBackupRestore\Commands\RestoreCommand;
 
-it('downloads remote backup, imports a single mysql dump into mysql database', function () {
+// MySQL
+it('restores mysql database', function (string $backup, ?string $password = null) {
     $this->artisan(RestoreCommand::class, [
         '--disk' => 'remote',
-        '--backup' => 'Laravel/2023-01-28-mysql-no-compression-no-encryption.zip',
+        '--backup' => $backup,
         '--database' => 'mysql',
+        '--password' => $password,
     ])->assertSuccessful();
 
     $result = DB::connection('mysql')->table('users')->count();
 
     expect($result)->toBe(10);
-});
-
-it('downloads remote backup, decrypts and imports a single mysql dump into mysql database', function () {
-    $this->artisan(RestoreCommand::class, [
-        '--disk' => 'remote',
-        '--backup' => 'Laravel/2023-01-28-mysql-no-compression-encrypted.zip',
-        '--database' => 'mysql',
-        '--password' => 'password',
-    ])->assertSuccessful();
-
-    $result = DB::connection('mysql')->table('users')->count();
-
-    expect($result)->toBe(10);
-});
-
-it('downloads and decompresses remote backup, imports a single mysql dump into mysql database', function () {
-    $this->artisan(RestoreCommand::class, [
-        '--disk' => 'remote',
-        '--backup' => 'Laravel/2023-01-28-mysql-compression-no-encryption.zip',
-        '--database' => 'mysql',
-    ])->assertSuccessful();
-
-    $result = DB::connection('mysql')->table('users')->count();
-
-    expect($result)->toBe(10);
-});
-
-it('downloads, decompresses remote backup, decrypts and imports a single mysql dump into mysql database', function () {
-    $this->artisan(RestoreCommand::class, [
-        '--disk' => 'remote',
-        '--backup' => 'Laravel/2023-01-28-mysql-compression-encrypted.zip',
-        '--database' => 'mysql',
-        '--password' => 'password',
-    ])->assertSuccessful();
-
-    $result = DB::connection('mysql')->table('users')->count();
-
-    expect($result)->toBe(10);
-});
+})->with([
+    [
+        'backup' => 'Laravel/2023-01-28-mysql-no-compression-no-encryption.zip',
+    ],
+    [
+        'backup' => 'Laravel/2023-01-28-mysql-compression-no-encryption.zip',
+    ],
+    [
+        'backup' => 'Laravel/2023-01-28-mysql-no-compression-encrypted.zip',
+        'password' => 'password',
+    ],
+    [
+        'backup' => 'Laravel/2023-01-28-mysql-compression-encrypted.zip',
+        'password' => 'password',
+    ],
+]);
 
 // sqlite
-it('restores database backup into sqlite database', function (string $backup, ?string $password = null) {
+it('restores sqlite database', function (string $backup, ?string $password = null) {
     $this->artisan(RestoreCommand::class, [
         '--disk' => 'remote',
         '--backup' => $backup,
@@ -70,11 +49,13 @@ it('restores database backup into sqlite database', function (string $backup, ?s
 })->with([
     [
         'backup' => 'Laravel/2023-02-28-sqlite-no-compression-no-encryption.zip',
-        'password' => null,
     ],
     [
         'backup' => 'Laravel/2023-02-28-sqlite-compression-no-encryption.zip',
-        'password' => null,
+    ],
+    [
+        'backup' => 'Laravel/2023-02-28-sqlite-no-compression-encrypted.zip',
+        'password' => 'password',
     ],
     [
         'backup' => 'Laravel/2023-02-28-sqlite-compression-encrypted.zip',
