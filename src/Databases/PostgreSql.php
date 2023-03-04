0fe2086f37099455c414dbbb20cc6e92634621d3
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-04 16:35:14 +0100

    Add PostgreSQL support

--- src/Databases/PostgreSql.php
+++ src/Databases/PostgreSql.php
@@ -4,8 +4,36 @@ declare(strict_types=1);
 
 namespace Wnx\LaravelBackupRestore\Databases;
 
+use Symfony\Component\Process\Process;
 use Wnx\LaravelBackupRestore\DbImporter;
 
 class PostgreSql extends DbImporter
 {
+    /**
+     * @throws ImportFailed
+     */
+    public function importToDatabase(string $dumpFile): void
+    {
+        $process = $this->getProcess($dumpFile);
+
+        $process->run();
+
+        $this->checkIfImportWasSuccessful($process, $dumpFile);
+    }
+
+    public function getImportCommand(string $dumpFile): string
+    {
+        if (str($dumpFile)->endsWith('gz')) {
+            return 'gunzip -c '.$dumpFile.' | psql -U '.config('database.connections.pgsql.username').' -d '.config('database.connections.pgsql.database');
+        }
+
+        return 'psql -U '.config('database.connections.pgsql.username').' -d '.config('database.connections.pgsql.database').' < '.$dumpFile;
+    }
+
+    private function getProcess(string $dumpFile): Process
+    {
+        $command = $this->getImportCommand($dumpFile);
+
+        return Process::fromShellCommandline($command, null, null, null, 0);
+    }
 }
