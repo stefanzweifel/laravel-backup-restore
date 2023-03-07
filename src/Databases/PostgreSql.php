b43e727978cb0f406dfc05ba44b987a13082a7df
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-07 19:54:42 +0100

    WIP

--- src/Databases/PostgreSql.php
+++ src/Databases/PostgreSql.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Wnx\LaravelBackupRestore\Databases;
 
+use Spatie\Backup\Tasks\Backup\DbDumperFactory;
 use Symfony\Component\Process\Process;
 use Wnx\LaravelBackupRestore\DbImporter;
 use Wnx\LaravelBackupRestore\Exceptions\ImportFailed;
@@ -24,6 +25,9 @@ class PostgreSql extends DbImporter
 
     public function getImportCommand(string $dumpFile): string
     {
+        $dumper = DbDumperFactory::createFromConnection('pgsql');
+        $dumper->getContentsOfCredentialsFile();
+
         if (str($dumpFile)->endsWith('gz')) {
             return 'gunzip -c '.$dumpFile.' | psql -U '.config('database.connections.pgsql.username').' -d '.config('database.connections.pgsql.database');
         }
