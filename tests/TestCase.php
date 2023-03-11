e718ed0efa6686face13f5c0f1c14c084f6555ab
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-11 12:08:47 +0100

    Remove unused configuration from TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,30 +10,11 @@ use Wnx\LaravelBackupRestore\LaravelBackupRestoreServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Wnx\\LaravelBackupRestore\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             LaravelBackupRestoreServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-backup-restore_table.php.stub';
-        $migration->up();
-        */
     }
 
     protected function defineEnvironment($app)
@@ -43,7 +24,6 @@ class TestCase extends Orchestra
         $app['config']->set('database.connections.sqlite', [
             'driver' => 'sqlite',
             'database' => 'database/database.sqlite',
-            'prefix' => '',
         ]);
 
         $app['config']->set('database.connections.mysql', [
