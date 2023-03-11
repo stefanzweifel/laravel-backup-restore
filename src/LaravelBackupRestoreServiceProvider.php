5febdcf73af4738045f702aaf180b0f3cd1406c9
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-03-11 11:54:59 +0100

    Cleanup ServiceProvider

--- src/LaravelBackupRestoreServiceProvider.php
+++ src/LaravelBackupRestoreServiceProvider.php
@@ -12,16 +12,9 @@ class LaravelBackupRestoreServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-backup-restore')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-backup-restore_table')
             ->hasCommand(RestoreCommand::class);
     }
 }
