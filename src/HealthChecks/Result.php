dbea94527b12d861719cd214cca87ef2dcf7d93a
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-05-20 11:40:24 +0200

    Add HealthCheck and Result classes

---
+++ src/HealthChecks/Result.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wnx\LaravelBackupRestore\HealthChecks;
+
+use Illuminate\Console\Command;
+
+class Result
+{
+    public function __construct(
+        readonly public HealthCheck $healthCheck,
+        public int $status,
+        public ?string $message,
+    ) {
+        //
+    }
+
+    public static function make(HealthCheck $healthCheck): self
+    {
+        return new self(healthCheck: $healthCheck, status: Command::SUCCESS, message: null);
+    }
+
+    public function ok(): self
+    {
+        $this->status = Command::SUCCESS;
+
+        return $this;
+    }
+
+    public function failed(string $message = null): self
+    {
+        $this->status = Command::FAILURE;
+
+        $this->message = $message;
+
+        return $this;
+    }
+}
