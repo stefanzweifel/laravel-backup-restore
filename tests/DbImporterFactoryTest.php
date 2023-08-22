e1b3a29e3c46f73d5cda8d93a3a0f10759e8b179
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2023-08-22 20:15:55 +0200

    Use Driver Name when creating DbImporter instead of Connection Name (#24)
    * Add Additional Database Connections
    
    * Update Test
    
    * Add Connection with unsupported Driver
    
    * Update Test and Exception
    
    * Update Implementation
    
    Fixes #22
    
    * Use *-restore connections in RestoreCommandTest

--- tests/DbImporterFactoryTest.php
+++ tests/DbImporterFactoryTest.php
@@ -5,23 +5,39 @@ declare(strict_types=1);
 use Wnx\LaravelBackupRestore\DbImporterFactory;
 use Wnx\LaravelBackupRestore\Exceptions\CannotCreateDbImporter;
 
-it('returns db importer instances for given database driver', function ($driver, $expectedClass) {
-    expect(DbImporterFactory::createFromConnection($driver))->toBeInstanceOf($expectedClass);
+it('returns db importer instances for given database driver', function ($connectionName, $expectedClass) {
+    expect(DbImporterFactory::createFromConnection($connectionName))->toBeInstanceOf($expectedClass);
 })->with([
     [
-        'driver' => 'mysql',
+        'connectionName' => 'mysql',
         'expected' => \Wnx\LaravelBackupRestore\Databases\MySql::class,
     ],
     [
-        'driver' => 'sqlite',
+        'connectionName' => 'mysql-restore',
+        'expected' => \Wnx\LaravelBackupRestore\Databases\MySql::class,
+    ],
+    [
+        'connectionName' => 'sqlite',
         'expected' => \Wnx\LaravelBackupRestore\Databases\Sqlite::class,
     ],
     [
-        'driver' => 'pgsql',
+        'connectionName' => 'sqlite-restore',
+        'expected' => \Wnx\LaravelBackupRestore\Databases\Sqlite::class,
+    ],
+    [
+        'connectionName' => 'pgsql',
+        'expected' => \Wnx\LaravelBackupRestore\Databases\PostgreSql::class,
+    ],
+    [
+        'connectionName' => 'pgsql-restore',
         'expected' => \Wnx\LaravelBackupRestore\Databases\PostgreSql::class,
     ],
 ]);
 
 it('throws exception if no db importer instance can be created for connection')
     ->tap(fn () => DbImporterFactory::createFromConnection('unsupported'))
+    ->throws(CannotCreateDbImporter::class);
+
+it('throws exception if no db importer instance can be created for driver')
+    ->tap(fn () => DbImporterFactory::createFromConnection('unsupported-driver'))
     ->throws(CannotCreateDbImporter::class);
