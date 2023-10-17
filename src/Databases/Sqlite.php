d2efdf99e28686b027dcc681d18d128b204622e8
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-10-17 20:47:51 +0200

    Remove Todo in Sqlite

--- src/Databases/Sqlite.php
+++ src/Databases/Sqlite.php
@@ -9,7 +9,6 @@ class Sqlite extends DbImporter
     public function getImportCommand(string $dumpFile, string $connection): string
     {
         // @todo: Improve detection of compressed files
-        // @todo: Use $pendingRestore->connection
         if (str($dumpFile)->endsWith('gz')) {
             // Shell command to import a gzipped SQL file to a sqlite database
             return 'gunzip -c '.$dumpFile.' | sqlite3 '.config("database.connections.{$connection}.database");
