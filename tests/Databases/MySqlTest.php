87f0aab7ac59a74e8a5f384502018faac59d2d2f
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2023-10-31 20:22:30 +0100

    Respect dump_binary_path setting when importing database (#40)
    * Add dumpBinaryPath to DbImporter
    
    * Set and use Custom Binary Path in MySql
    
    * Set and use Custom Binary Path in PostgreSql
    
    * Disable Dependencies Check
    
    * Use DIRECTORY_SEPARATOR when setting Binary Path
    
    * Add Connections with Custom Binary Path
    
    * Add Process Tests

--- tests/Databases/MySqlTest.php
+++ tests/Databases/MySqlTest.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Illuminate\Process\PendingProcess;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Process;
 use Wnx\LaravelBackupRestore\Databases\MySql;
 use Wnx\LaravelBackupRestore\Events\DatabaseDumpImportWasSuccessful;
 use Wnx\LaravelBackupRestore\Exceptions\ImportFailed;
+
+use function PHPUnit\Framework\assertStringContainsString;
 
 it('imports mysql dump', function (string $dumpFile) {
     Event::fake();
@@ -23,6 +27,73 @@ it('imports mysql dump', function (string $dumpFile) {
     __DIR__.'/../storage/Laravel/2023-01-28-mysql-no-compression-no-encryption.sql',
     __DIR__.'/../storage/Laravel/2023-01-28-mysql-compression-no-encryption.sql.gz',
 ]);
+
+it('uses default binary path to import mysql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-01-28-mysql-no-compression-no-encryption.sql';
+
+    app(MySql::class)->importToDatabase(
+        dumpFile: $dumpFile,
+        connection: 'mysql'
+    );
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringContainsString("'mysql'", $process->command);
+
+        return true;
+    });
+
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+});
+
+it('uses custom binary path to import mysql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-01-28-mysql-no-compression-no-encryption.sql';
+
+    app(MySql::class)->importToDatabase(
+        dumpFile: $dumpFile,
+        connection: 'mysql-restore-binary-path'
+    );
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringContainsString('/usr/bin/mysql', $process->command);
+
+        return true;
+    });
+
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+});
+
+it('uses custom binary path to import compressed mysql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-01-28-mysql-compression-no-encryption.sql.gz';
+
+    app(MySql::class)->importToDatabase(
+        dumpFile: $dumpFile,
+        connection: 'mysql-restore-binary-path'
+    );
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringContainsString('gunzip <', $process->command);
+        assertStringContainsString('/usr/bin/mysql', $process->command);
+
+        return true;
+    });
+
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+});
 
 it('throws import failed exception if mysql dump could not be imported')
     ->tap(fn () => app(MySql::class)->importToDatabase('file-does-not-exist', 'mysql'))
--- tests/Databases/PostgreSqlTest.php
+++ tests/Databases/PostgreSqlTest.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
+use Illuminate\Process\PendingProcess;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Process;
 use Wnx\LaravelBackupRestore\Databases\PostgreSql;
 use Wnx\LaravelBackupRestore\Events\DatabaseDumpImportWasSuccessful;
 use Wnx\LaravelBackupRestore\Exceptions\ImportFailed;
+
+use function PHPUnit\Framework\assertStringContainsString;
+use function PHPUnit\Framework\assertStringNotContainsString;
 
 it('imports pgsql dump', function (string $dumpFile) {
     Event::fake();
@@ -23,6 +28,71 @@ it('imports pgsql dump', function (string $dumpFile) {
     __DIR__.'/../storage/Laravel/2023-03-04-pgsql-no-compression-no-encryption.sql',
     __DIR__.'/../storage/Laravel/2023-03-04-pgsql-compression-no-encryption.sql.gz',
 ])->group('pgsql');
+
+it('uses default binary to import pgsql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-03-04-pgsql-no-compression-no-encryption.sql';
+
+    app(PostgreSql::class)->importToDatabase($dumpFile, 'pgsql');
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringNotContainsString('/usr/bin/psql', $process->command);
+        assertStringContainsString('psql', $process->command);
+
+        return true;
+    });
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+
+    $result = DB::connection('pgsql')->table('users')->count();
+    expect($result)->toBe(10);
+})->group('pgsql');
+
+it('uses custom binary to import pgsql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-03-04-pgsql-no-compression-no-encryption.sql';
+
+    app(PostgreSql::class)->importToDatabase($dumpFile, 'pgsql-restore-binary-path');
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringContainsString('/usr/bin/psql', $process->command);
+
+        return true;
+    });
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+
+    $result = DB::connection('pgsql')->table('users')->count();
+    expect($result)->toBe(10);
+})->group('pgsql');
+
+it('uses custom binary to import compressed pgsql dump', function () {
+    Event::fake();
+    Process::fake();
+
+    $dumpFile = __DIR__.'/../storage/Laravel/2023-03-04-pgsql-compression-no-encryption.sql.gz';
+
+    app(PostgreSql::class)->importToDatabase($dumpFile, 'pgsql-restore-binary-path');
+
+    Process::assertRan(function (PendingProcess $process) {
+        assertStringContainsString('gunzip -c', $process->command);
+        assertStringContainsString('/usr/bin/psql', $process->command);
+
+        return true;
+    });
+    Event::assertDispatched(function (DatabaseDumpImportWasSuccessful $event) use ($dumpFile) {
+        return $event->absolutePathToDump === $dumpFile;
+    });
+
+    $result = DB::connection('pgsql')->table('users')->count();
+    expect($result)->toBe(10);
+})->group('pgsql');
 
 it('throws import failed exception if pgsql dump could not be imported')
     ->tap(fn () => app(PostgreSql::class)->importToDatabase('file-does-not-exist', 'pgsql'))
