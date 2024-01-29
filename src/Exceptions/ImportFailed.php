d6d90659a9f34f782fcfdfd1b77c49bace819365
Alies Lapatsin <web.lapateen@gmail.com>
2024-01-29 21:05:30 +0530

    Support bz2 dump files

--- src/Exceptions/ImportFailed.php
+++ src/Exceptions/ImportFailed.php
@@ -16,6 +16,11 @@ class ImportFailed extends Exception
         return new static("The import process failed with a none successful exitcode.{$processOutput}");
     }
 
+    public static function decompressionFailed(string $reason, string $filename): static
+    {
+        return new static("Could not decompress $filename dump file: $reason");
+    }
+
     protected static function formatProcessOutput(ProcessResult $process): string
     {
         $output = $process->output() ?: '<no output>';
