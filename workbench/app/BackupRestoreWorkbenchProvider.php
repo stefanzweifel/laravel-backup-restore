4666ebe15d515427f81ffe5bf3c0691f5c8769ca
Pieter Claerhout <pieter@yellowduck.be>
2024-08-07 11:41:19 +0200

    Added support for spatie/laravel-backup version 9
    This required all config settings to be present as described in the [upgrade guide](https://github.com/spatie/laravel-backup/blob/main/UPGRADING.md)

--- workbench/app/BackupRestoreWorkbenchProvider.php
+++ workbench/app/BackupRestoreWorkbenchProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Workbench\App;
 
 use Illuminate\Support\ServiceProvider;
+use ZipArchive;
 
 class BackupRestoreWorkbenchProvider extends ServiceProvider
 {
@@ -73,14 +74,30 @@ class BackupRestoreWorkbenchProvider extends ServiceProvider
             'backup' => [
                 'name' => env('APP_NAME', 'Laravel'),
                 'source' => [
+                    'files' => [
+                        'include' => [
+                            base_path(),
+                        ],
+                        'exclude' => [
+                            base_path('vendor'),
+                            base_path('node_modules'),
+                        ],
+                        'follow_links' => false,
+                        'ignore_unreadable_directories' => false,
+                        'relative_path' => null,
+                    ],
                     'databases' => [
                         'mysql',
                     ],
                 ],
                 'database_dump_compressor' => \Spatie\DbDumper\Compressors\GzipCompressor::class,
+                'database_dump_file_timestamp_format' => null,
+                'database_dump_filename_base' => 'database',
                 'database_dump_file_extension' => '',
 
                 'destination' => [
+                    'compression_method' => ZipArchive::CM_DEFAULT,
+                    'compression_level' => 9,
                     'filename_prefix' => '',
                     'disks' => [
                         'remote',
@@ -89,6 +106,60 @@ class BackupRestoreWorkbenchProvider extends ServiceProvider
                 'temporary_directory' => storage_path('app/backup-temp'),
                 'password' => env('BACKUP_ARCHIVE_PASSWORD'),
                 'encryption' => 'default',
+                'tries' => 1,
+                'retry_delay' => 0,
+            ],
+            'notifications' => [
+                'notifications' => [
+                    \Spatie\Backup\Notifications\Notifications\BackupHasFailedNotification::class => ['mail'],
+                    \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFoundNotification::class => ['mail'],
+                    \Spatie\Backup\Notifications\Notifications\CleanupHasFailedNotification::class => ['mail'],
+                    \Spatie\Backup\Notifications\Notifications\BackupWasSuccessfulNotification::class => ['mail'],
+                    \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFoundNotification::class => ['mail'],
+                    \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessfulNotification::class => ['mail'],
+                ],
+                'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
+                'mail' => [
+                    'to' => 'your@example.com',
+                    'from' => [
+                        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+                        'name' => env('MAIL_FROM_NAME', 'Example'),
+                    ],
+                ],
+                'slack' => [
+                    'webhook_url' => '',
+                    'channel' => null,
+                    'username' => null,
+                    'icon' => null,
+                ],
+                'discord' => [
+                    'webhook_url' => '',
+                    'username' => '',
+                    'avatar_url' => '',
+                ],
+            ],
+            'monitor_backups' => [
+                [
+                    'name' => env('APP_NAME', 'laravel-backup'),
+                    'disks' => ['local'],
+                    'health_checks' => [
+                        \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
+                        \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+                    ],
+                ],
+            ],
+            'cleanup' => [
+                'strategy' => \Spatie\Backup\Tasks\Cleanup\Strategies\DefaultStrategy::class,
+                'default_strategy' => [
+                    'keep_all_backups_for_days' => 7,
+                    'keep_daily_backups_for_days' => 16,
+                    'keep_weekly_backups_for_weeks' => 8,
+                    'keep_monthly_backups_for_months' => 4,
+                    'keep_yearly_backups_for_years' => 2,
+                    'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
+                ],
+                'tries' => 1,
+                'retry_delay' => 0,
             ],
         ]);
     }
