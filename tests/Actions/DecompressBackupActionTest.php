853ba71f3d33ddcf3a771a3d787d3445890062f1
Stefan Zweifel <stefanzweifel@users.noreply.github.com>
2024-10-20 14:44:51 +0200

    Use local.root value in getAbsolutePathToLocalDecompressedBackup (#80)

--- tests/Actions/DecompressBackupActionTest.php
+++ tests/Actions/DecompressBackupActionTest.php
@@ -25,6 +25,25 @@ it('decompresses zip backup file without password', function () {
     Storage::assertExists($pendingRestore->getPathToLocalDecompressedBackup());
 });
 
+it('decompresses zip backup file without password if local root differs from default', function () {
+    config(['filesystems.disks.local.root' => storage_path('app/private')]);
+
+    $pendingRestore = PendingRestore::make(
+        disk: 'remote',
+        backup: 'Laravel/2023-01-28-mysql-no-compression-no-encryption.zip',
+        connection: 'mysql',
+        backupPassword: null,
+    );
+
+    Storage::assertMissing($pendingRestore->getPathToLocalCompressedBackup());
+    app(DownloadBackupAction::class)->execute($pendingRestore);
+    Storage::assertExists($pendingRestore->getPathToLocalCompressedBackup());
+
+    Storage::assertMissing($pendingRestore->getPathToLocalDecompressedBackup());
+    app(DecompressBackupAction::class)->execute($pendingRestore);
+    Storage::assertExists('private'.DIRECTORY_SEPARATOR.$pendingRestore->getPathToLocalDecompressedBackup());
+});
+
 it('decompresses zip backup file that needs password do decrypt', function () {
     $pendingRestore = PendingRestore::make(
         disk: 'remote',
